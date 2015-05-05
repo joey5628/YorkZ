79f5af094229c1036c9cba18e6207d80dd86a3b7
yi.z <yi.z@Ctrip.com>
2015-05-05 13:56:05 +0800

    yorkz update window

--- content/index.php
+++ content/index.php
@@ -8,7 +8,20 @@
 	<?php $fed->allCss(); ?>
 </head>
 <body>
-	yi
+	<div class="site-header">
+		<div class="header-box">
+			<img src="../img/sun.jpg" alt="" class="header-pic">
+			<h1>
+				<a href="/">YorkZ</a>
+			</h1>
+		</div>
+	</div>
+	<div class="container">
+		container
+	</div>
+	<div class="site-footer">
+		footer
+	</div>
 	<?php $fed->allJs(); ?>
 </body>
 </html>
