c42169cc80e24e6c619289789c6fa791e28b55e4
yi.z <yi.z@Ctrip.com>
2015-05-06 18:21:34 +0800

    yorkz update for window

--- content/index.php
+++ content/index.php
@@ -8,7 +8,29 @@
 	<?php $fed->allCss(); ?>
 </head>
 <body>
-	<div class="site-header">
+	<header class="header">
+		<div class="inner">
+			<div class="logo">
+				<a href="/">
+					<img src="../img/sun.jpg" alt="" class="logo-img">
+					<h1 class="logo-name">YorkZ&nbsp;-</h1>
+					<h1 class="logo-description">专注前端开发和用户体验</h1>
+				</a>
+			</div>
+		</div>
+	</header>
+	<div class="container">
+	
+	</div>
+	<footer class="footer">
+		<div class="inner">
+			<div class="footer-info">
+				© 2015
+				<a href="/" target="_blank">YorkZ</a>
+			</div>
+		</div>
+	</footer>
+	<!-- <div class="site-header">
 		<div class="header-box">
 			<img src="../img/sun.jpg" alt="" class="header-pic">
 			<h1>
@@ -21,7 +43,7 @@
 	</div>
 	<div class="site-footer">
 		footer
-	</div>
+	</div> -->
 	<?php $fed->allJs(); ?>
 </body>
 </html>
